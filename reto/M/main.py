13688b7c816c0ce5a51e6359bce972207099866e
Leonardoalv201 <a01705998@itesm.mx>
2022-09-13 23:54:29 -0500

    update flask

--- reto/M/main.py
+++ reto/M/main.py
@@ -1,19 +1,23 @@
 from flask import Flask, request, jsonify
-import leo
+import pandas as pd
+import M.leo as leo
 
 app = Flask(_name_)
 
 @app.route("/")
 def hello_world():
   args = request.args
-  nbr_store_input = args.get('nbr_store_input')
-  family_input = args.get('family_input')
-  onpromotion_input = args.get('onpromotion_input')
-  its_holiday = args.get('its_holiday')
-  if leo._model is None:
+  nbr_store_input = args.get('store', type=int)
+  family_input = args.get('family', type=int)
+  onpromotion_input = args.get('onpromotion', type=int)
+  its_holiday = args.get('its_holiday', type=int)
+  model = leo.model()
+  if model is None:
     return "Model is not trained", 400
+
+  print(nbr_store_input, family_input, onpromotion_input, its_holiday)
   
-  pred = leo.predict([[nbr_store_input, family_input, onpromotion_input, its_holiday]])
+  pred = leo.predict(pd.DataFrame([[nbr_store_input, family_input, onpromotion_input, its_holiday]], columns=['store_nbr', 'family', 'onpromotion', 'type']))
   return jsonify({"prediction": pred.tolist()})
 
-leo.train()
+# leo.train()
