e7e0204f3e330c03fe67ce7a192e2bd8ea18ce75
Leonardoalv201 <a01705998@itesm.mx>
2022-09-13 23:54:29 -0500

    update flask

--- reto/M/leo.py
+++ reto/M/leo.py
@@ -1,14 +1,17 @@
 import xgboost as xgb
+global _model
 
-_model = None
+xbg_reg = xgb.XGBRegressor()
 
-def train():
-  xbg_reg = xgb.XGBRegressor()
-
-  xbg_reg.load_model("model.txt")
-  _model = xbg_reg
+xbg_reg.load_model("model.json")
+_model = xbg_reg
+print('loaded model', _model)
 
 def predict(test):
   if _model is None:
     return None
   return _model.predict(test)
+
+
+def model():
+  return _model
